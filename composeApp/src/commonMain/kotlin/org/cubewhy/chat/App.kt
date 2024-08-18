f924d649d07dd30f346746fe2acee800af79e10f
cubewhy <cubewhy@163.com>
2024-08-18 13:25:07 +0800

    feat: snackbar

--- composeApp/src/commonMain/kotlin/org/cubewhy/chat/App.kt
+++ composeApp/src/commonMain/kotlin/org/cubewhy/chat/App.kt
@@ -9,18 +9,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 import org.cubewhy.chat.theme.QMessengerTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -42,52 +46,64 @@ fun LoginForm(modifier: Modifier = Modifier) {
     var password by remember { mutableStateOf("") }
     var passwordError by remember { mutableStateOf(false) }
 
-    Column(
-        modifier = Modifier.fillMaxWidth(0.4f).padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Login", fontSize = 32.sp, color = MaterialTheme.colorScheme.onBackground)
-        Spacer(modifier = Modifier.height(16.dp))
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-        TextField(
-            value = username,
-            placeholder = {
-                Text("Username")
-            },
-            onValueChange = {
-                username = it
-            },
-            modifier = Modifier.fillMaxWidth().padding(8.dp)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        TextField(
-            value = password,
-            placeholder = {
-                Text("Password")
-            },
-            onValueChange = {
-                password = it
-                passwordError = password.length < 6
-            },
-            modifier = Modifier.fillMaxWidth().padding(8.dp)
-        )
-        if (passwordError) {
-            Text(text = "Password must be at least 6 characters", color = Color.Red)
+    Scaffold(
+        modifier = Modifier.fillMaxWidth(0.6f).padding(16.dp),
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
         }
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Login", fontSize = 32.sp, color = MaterialTheme.colorScheme.onBackground)
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+            TextField(
+                value = username,
+                placeholder = {
+                    Text("Username")
+                },
+                onValueChange = {
+                    username = it
+                },
+                modifier = Modifier.fillMaxWidth().padding(8.dp)
+            )
 
-        Button(onClick = {
-            if (passwordError || username.isEmpty() || password.isEmpty()) {
-                // Show error or handle validation
-            } else {
-                // Perform login
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TextField(
+                value = password,
+                placeholder = {
+                    Text("Password")
+                },
+                onValueChange = {
+                    password = it
+                    passwordError = password.length < 6
+                },
+                modifier = Modifier.fillMaxWidth().padding(8.dp)
+            )
+            if (passwordError) {
+                Text(text = "Password must be at least 6 characters", color = Color.Red)
             }
-        }) {
-            Text(text = "Login")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                scope.launch {
+                    if (passwordError || username.isEmpty() || password.isEmpty()) {
+                        snackbarHostState.showSnackbar("Incorrect password length")
+                    } else {
+                        // Perform login
+                        snackbarHostState.showSnackbar("OK")
+                    }
+                }
+            }) {
+                Text(text = "Login")
+            }
         }
     }
 }
