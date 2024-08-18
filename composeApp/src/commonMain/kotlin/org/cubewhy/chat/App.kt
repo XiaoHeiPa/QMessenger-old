f576671cf527ed5ebdc0ac082bb9ba3425353b8c
cubewhy <cubewhy@163.com>
2024-08-18 13:49:55 +0800

    feat: animation on auth failed

--- composeApp/src/commonMain/kotlin/org/cubewhy/chat/App.kt
+++ composeApp/src/commonMain/kotlin/org/cubewhy/chat/App.kt
@@ -1,12 +1,30 @@
 package org.cubewhy.chat
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.repeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.shrinkOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -14,6 +32,7 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,6 +43,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import kotlinx.coroutines.launch
 import org.cubewhy.chat.theme.QMessengerTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -46,16 +67,39 @@ fun LoginForm(modifier: Modifier = Modifier) {
     var password by remember { mutableStateOf("") }
     var passwordError by remember { mutableStateOf(false) }
 
+    var showSwitchServerDialog by remember { mutableStateOf(false) }
+
     val scope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
 
+    val shakeAnim = remember {
+        Animatable(0f)
+    }
+
     Scaffold(
-        modifier = Modifier.fillMaxWidth(0.6f).padding(16.dp),
+        modifier = Modifier.fillMaxSize(),
         snackbarHost = {
             SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+            AnimatedVisibility(
+                visible = !showSwitchServerDialog,
+                enter = slideInVertically() + expandIn(),
+                exit = slideOutVertically() + shrinkOut()
+            ) {
+                FloatingActionButton(onClick = {
+                    showSwitchServerDialog = true
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.Edit,
+                        contentDescription = "Edit"
+                    )
+                }
+            }
         }
-    ) {
+    ) { inn ->
         Column(
+            modifier = Modifier.padding(30.dp).fillMaxSize().padding(inn),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -92,17 +136,57 @@ fun LoginForm(modifier: Modifier = Modifier) {
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(onClick = {
-                scope.launch {
-                    if (passwordError || username.isEmpty() || password.isEmpty()) {
-                        snackbarHostState.showSnackbar("Incorrect password length")
-                    } else {
-                        // Perform login
-                        snackbarHostState.showSnackbar("OK")
+            Button(
+                modifier = Modifier.offset(x = shakeAnim.value.dp),
+                onClick = {
+                    scope.launch {
+                        if (passwordError || username.isEmpty() || password.isEmpty()) {
+                            shakeAnim.animateTo(
+                                targetValue = 10f,
+                                animationSpec = repeatable(
+                                    iterations = 5,
+                                    animation = tween(
+                                        durationMillis = 100,
+                                        easing = LinearEasing
+                                    )
+                                ),
+                                initialVelocity = 0f
+                            )
+                            shakeAnim.snapTo(0f)
+                        } else {
+                            // Perform login
+                            snackbarHostState.showSnackbar("OK")
+                        }
+                    }
+                }) {
+                Text(text = "Login")
+            }
+        }
+
+        if (showSwitchServerDialog) {
+            Dialog(
+                onDismissRequest = { showSwitchServerDialog = false },
+                properties = DialogProperties(
+                    dismissOnBackPress = true,
+                    dismissOnClickOutside = true
+                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(16.dp)
+
+                ) {
+                    Column(
+                        modifier = Modifier.align(Alignment.Center),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text("Server Settings", fontSize = 24.sp)
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = { /* Handle server switch */ }) {
+                            Text("Switch Server")
+                        }
                     }
                 }
-            }) {
-                Text(text = "Login")
             }
         }
     }
